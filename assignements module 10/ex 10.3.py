f0be6cdf683a15efe45461eae0f5a4c67c311d1b
gaetanometropolia <gaetano.trigili@metropolia.fi>
2026-03-18 10:58:13 +0200

    folders added

---
+++ assignements module 10/ex 10.3.py
@@ -0,0 +1,1 @@
+# Extend the program again by adding a method fire_alarm that does not receive any parameters and moves all elevators to the bottom floor. Continue the main program by causing a fire alarm in your building.
