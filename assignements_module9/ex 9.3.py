0890bcf6d7c6405882a21b425878dd556d9b1150
gaetanometropolia <gaetano.trigili@metropolia.fi>
2026-03-19 19:30:17 +0200

    exercise successfull

--- assignements_module9/ex 9.3.py
+++ assignements_module9/ex 9.3.py
@@ -1,1 +1,31 @@
 # Again, extend the program by adding a new drive method that receives the number of hours as a parameter. The method increases the travelled distance by how much the car has travelled in constant speed in the given time. Example: The travelled distance of car object is 2000 km. The current speed is 60 km/h. Method call car.drive(1.5) increases the travelled distance to 2090 km.
+
+class Car:
+
+    def __init__(self, registration_number, maximum_speed):
+        self.registration_number = registration_number
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+
+    def accelerate(self, speed_change):
+        self.current_speed += speed_change
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+
+    def drive (self, hours):
+        distance_covered = self.current_speed * hours
+        self.travelled_distance += distance_covered
+
+new_car = Car("ABC-123", 142)
+
+new_car.accelerate(60)
+
+new_car.travelled_distance = 2000
+new_car.drive(1.5)
+
+print(f"The new updated car travel distance is {new_car.travelled_distance}")
+
